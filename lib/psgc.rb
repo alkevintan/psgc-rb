d8b7496c1b0fdc8463b3ba29fbc4130069f25ef2
Al Kevin Tan <alkevintan@gmail.com>
2026-04-25 11:13:10 +0800

    feat: add finder methods for searching regions, provinces, cities, barangays

--- lib/psgc.rb
+++ lib/psgc.rb
@@ -33,4 +33,33 @@ module Psgc
 
     JSON.parse(File.read(file_path), symbolize_names: true)
   end
+
+  def self.find_region(code: nil, name: nil)
+    return nil unless code || name
+    find(regions, code: code, name: name)
+  end
+
+  def self.find_province(code: nil, name: nil, region_code: nil)
+    return nil unless code || name || region_code
+    find(provinces, code: code, name: name, region_code: region_code)
+  end
+
+  def self.find_city_municipality(code: nil, name: nil, province_code: nil)
+    return nil unless code || name || province_code
+    find(cities_municipalities, code: code, name: name, province_code: province_code)
+  end
+
+  def self.find_barangay(code: nil, name: nil, city_municipality_code: nil)
+    return nil unless code || name || city_municipality_code
+    find(barangays, code: code, name: name, city_municipality_code: city_municipality_code)
+  end
+
+  def self.find(collection, code: nil, name: nil, **attrs)
+    collection.each do |item|
+      return item if code && item[:code] == code
+      return item if name && item[:name].to_s.downcase.include?(name.to_s.downcase)
+      attrs.each { |k, v| return item if item[k] == v && v }
+    end
+    nil
+  end
 end
--- test/psgc_test.rb
+++ test/psgc_test.rb
@@ -27,4 +27,28 @@ class PsgcTest < Minitest::Test
     assert_kind_of Array, Psgc.barangays
     refute_empty Psgc.barangays
   end
+
+  def test_find_region_by_code
+    region = Psgc.find_region(code: "1300000000")
+    refute_nil region
+    assert_equal "National Capital Region (NCR)", region[:name]
+  end
+
+  def test_find_region_by_name
+    region = Psgc.find_region(name: "National Capital Region")
+    refute_nil region
+    assert_equal "1300000000", region[:code]
+  end
+
+  def test_find_province
+    province = Psgc.find_province(code: "1400100000")
+    refute_nil province
+    assert_equal "Abra", province[:name]
+  end
+
+  def test_find_barangay
+    barangay = Psgc.find_barangay(code: "1403208016")
+    refute_nil barangay
+    assert_equal "Bagtayan", barangay[:name]
+  end
 end
